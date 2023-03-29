7d02a52a95e9cfe049f4bfe1c43a547cfb553d1b
Albert Salgueda <salguedainsti@gmail.com>
2023-03-29 19:16:03 +0200

    updated readme

--- README.md
+++ README.md
@@ -2,26 +2,22 @@
 
 Maia Teams, a Python library, leverages OpenAI's GPT-3.5-based AI chatbot, Maia, to create and test simple programs. It simulates Maia's self-conversations as a "programmer" and "product owner," iterating until completion or time expiration. A separate chat tests the code.
 
-
-# Installation
-To install Maia Teams, use pip:
-
-```
-pip install maiateams
-```
 # Usage
 First, create a file called .env and enter your OpenAI API information:
 ```
 OPENAI_API_KEY=sk-<YOUR-API-KEY>
 OPENAI_ORG=org-<YOUR-ORG>
 ```
-Then, set the project you would like Maia to work on by changing PROJECT in src/common.py. You can also change other settings there.
+Then, set the project you would like Maia to work on by changing PROJECT and PROJECT_NAME in src/common.py. 
+You can also change other settings there ( like model... )
 
 Finally, run the following command to start Maia:
 
 ```
-python -m maiateams
+python src/main.py
 ```
+
+Output will be printed and save in db/PROJECT_NAME.log
 
 # Issues
 The main issue right now is that Maia seems to give up a little too easily, and
--- src/common.py
+++ src/common.py
@@ -5,7 +5,7 @@ from typing import Dict, List
 MODEL = "gpt-3.5-turbo"
 Conversation = List[Dict[str, str]]
 PROJECT = "a powerful GPT-based chatbot API for generating data visualizations on-the-fly. This API enables users to import datasets in various formats, ask natural language queries, and receive tailored graphs in response. With customizable graph types, data filtering and aggregation, and AI-driven insights, the chatbot makes it easy to visualize and analyze data. Integration with popular messaging platforms and export options for sharing the generated graphs ensure seamless collaboration"
-
+PROJECT_NAME = "ChartBot"
 
 def print_message(message: Dict[str, str], role: str = None):
     write_message(message, role)
@@ -17,7 +17,7 @@ def write_message(message: dict, role: str = None):
     Writes a message to a file called 'conversation.log'
     Code messages have a [CODE] prefix to distinguish them from other messages
     """
-    with open("tests/conversation.log", "a") as f:
+    with open(f"db/{PROJECT_NAME}.log", "a") as f:
         if "```" in message["content"]:
             f.write("[CODE] ")
         f.write(f"{role.upper()}: {message['content']}\n")
