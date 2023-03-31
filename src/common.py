25754aac21519f6f331b04d4e6ac554e731dacf9
Albert Salgueda <salguedainsti@gmail.com>
2023-03-31 23:18:34 +0200

    more examples

--- src/common.py
+++ src/common.py
@@ -1,11 +1,10 @@
 # This conversation type should exclude the system message _and_ the first user message (containing information specific to each AI)
 from typing import Dict, List
 
-
 MODEL = "gpt-3.5-turbo"
 Conversation = List[Dict[str, str]]
-PROJECT = "a powerful GPT-based chatbot API for generating data visualizations on-the-fly. This API enables users to import datasets in various formats, ask natural language queries, and receive tailored graphs in response. With customizable graph types, data filtering and aggregation, and AI-driven insights, the chatbot makes it easy to visualize and analyze data. Integration with popular messaging platforms and export options for sharing the generated graphs ensure seamless collaboration"
-PROJECT_NAME = "ChartBot"
+PROJECT = "A program to control stock using bar codes"
+PROJECT_NAME = "stock_control"
 
 def print_message(message: Dict[str, str], role: str = None):
     write_message(message, role)
